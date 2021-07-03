20145c2f3d039e06b1a68151c2467ad6f0e0a6b1
Sophealin-Hul <80858775+Sophealin-Hul@users.noreply.github.com>
2021-07-03 17:58:34 -0400

    added files into week4

---
+++ week-four/weekFourAssignmentPt1/app.js
@@ -0,0 +1,15 @@
+axios.get("http://api.bryanuniversity.edu/sophealin/list/")
+    .then(response => {
+        for(let i = 0; i < response.data.length; i++) {
+            const list = document.createElement("li");
+            list.textContent = response.data[i].name;
+            document.body.appendChild(list);
+            if(response.data[i].isComplete === true) {
+                list.textContent = response.data[i].name.strike();
+            }
+            else if (response.data[i].isComplete === false) {
+                list.textContent = response.data[i].name;
+            }
+        }
+    })
+    .catch(error => console.log(error));
